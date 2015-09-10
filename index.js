9aacef0cbf8fde5cf6aab5233d413e0fcded2b56
Blain Smith <rebelgeek@blainsmith.com>
2015-09-10 14:18:26 -0400

    usage examples

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var chalk = require('chalk');
-var tinypass = require('./lib/tinypass').createClient({ aid: '8bc847a6-d1b7-4014-a043-c8b8e33418a9', privateKey: 'zqZABUNnGzUuo2bCFJKxXBddVqDmoyD8' });
+var tinypass = require('./lib/tinypass').createClient({ aid: '8bc847a6-d1b7-4014-a043-c8b8e33418a9', privateKey: 'zqZABUNnGzUuo2bCF' });
 
 var request = 'GET /r2/access?rid=[RID]&user_ref=[USER_REF]'
 
 console.log('Request String: ' + chalk.blue(request));
 console.log('Tinypass Auth Header: ' + chalk.green(tinypass.authorizationHeader(request)));
+console.log('Tinypass Encrypt: ' + chalk.green(tinypass.encrypt(request)));
