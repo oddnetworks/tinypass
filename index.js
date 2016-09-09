7f9a4161dfb1deec47922bd44cfcd0a35b5f2b99
Blain Smith <rebelgeek@blainsmith.com>
2016-09-09 13:12:09 -0400

    playground of methods

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var chalk = require('chalk');
-var tinypass = require('./lib/tinypass').createClient({ aid: '8bc847a6-d1b7-4014-a043-c8b8e33418a9', privateKey: 'zqZABUNnGzUuo2bCF' });
+var tinypass = require('./lib/tinypass').createClient({ aid: 'AID', privateKey: 'KEY' });
 
 var request = 'GET /r2/access?rid=[RID]&user_ref=[USER_REF]'
+var data = 'BwVjNeGVpcoO13Dn1LS6WqWfYMXKr3WkUOTTAK_zz5zWiA2vdGqRO1Mx1L3PTS4oClKfRXnnde-wFP6wUzJ6Z79k_SBT4cCF8kKL1BA-isxsqRHmvO4WwZtBhGsyGWlLQni-BxSVeTUrz4uqYwAfV7lZx5UwyI3T8e5MUfm8mOQR9x7pSfRWBiXR_fgQfhBnDTCLfsVfB929Usl4UE7axthPYiIxfssxqPG4N36ySmt9escBf0DohDUcYrLVSR99InXLw-vq7mhN58M4BY7pCA~~~iIynfdNYmP_GuXpMCImuIPpMWMwZlKJm9FDg7QipCJI';
+var text = new Buffer('odd networks');
 
 console.log('Request String: ' + chalk.blue(request));
 console.log('Tinypass Auth Header: ' + chalk.green(tinypass.authorizationHeader(request)));
 console.log('Tinypass Encrypt: ' + chalk.green(tinypass.encrypt(request)));
+console.log('Tinypass Decrypt: ' + chalk.green(tinypass.decrypt(tinypass.encrypt(request))));
+console.log('Tinypass Encode: ' + chalk.green(tinypass.encode(text)));
+console.log('Tinypass Decode: ' + chalk.green(tinypass.decode(tinypass.encode(text))));
